070dc7789736e27852a7a816724d3b884fe9f26d
Troy Connor <troy0820@users.noreply.github.com>
2016-09-28 13:14:57 -0400

    [main]Added React stuff to main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -23,7 +23,30 @@ import PerfectHTTPServer
 
 // Create HTTP server.
 let server = HTTPServer()
+var reactstuff = "<html><head><meta charset=\"UTF-8\" /><title>Hello React</title><script src=\"https://unpkg.com/react@15.3.2/dist/react.js\"></script><script src=\"https://unpkg.com/react-dom@15.3.2/dist/react-dom.js\"></script><script src=\"https://unpkg.com/babel-core@5.8.38/browser.min.js\"></script></head>"
++ "<body><div id=\"example\"></div><script type=\"text/babel\">"
 
++	"var HelloWorld = React.createClass({"
++ "render: function() {"
++ "return ("
+	+ "<p>"
+		+ "Hello, there This is React on Swift 3.0! <br/><br/>"
+		+ "It is {this.props.date.toTimeString()}"
+	+ "</p>"
++ ");"
++ "}"
++ "});"
+
++ "setInterval(function() {"
++ "ReactDOM.render("
++ "<HelloWorld date={new Date()} />,"
++ "document.getElementById('example')"
++ ");"
++ "}, 500);"
+
+	+ "</script>"
++ "</body>"
++ "</html>"
 // Register your own routes and handlers
 var routes = Routes()
 routes.add(method: .get, uri: "/", handler: {
@@ -34,6 +57,13 @@ routes.add(method: .get, uri: "/", handler: {
 	}
 )
 
+routes.add(method: .get, uri: "/react", handler: {
+	request, response in
+	response.setHeader(.contentType, value: "text/html")
+	response.appendBody(string: "<!DOCTYPE html>\(reactstuff)")
+	response.completed()
+	}
+)
 // Add the routes to the server.
 server.addRoutes(routes)
 
